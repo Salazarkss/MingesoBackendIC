753fabde73f1d15d3b764280c364bac6a0ab5dc9
asdfghjkln1 <38445887+asdfghjkln1@users.noreply.github.com>
2019-05-07 14:05:43 -0400

    fix cors 4

--- src/main/java/com/mingeso/ic/MingesoIcApplication.java
+++ src/main/java/com/mingeso/ic/MingesoIcApplication.java
@@ -10,6 +10,10 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
 import java.util.Collections;
 
 @SpringBootApplication(scanBasePackages="com.mingeso.ic")
