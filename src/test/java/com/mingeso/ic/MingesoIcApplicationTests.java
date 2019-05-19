3d0105c8d1df6b1a84291257cbed841e14d32433
Soul <vicente.rivera.r@usach.cl>
2019-05-19 01:41:57 -0400

    fix tests

--- src/test/java/com/mingeso/ic/MingesoIcApplicationTests.java
+++ src/test/java/com/mingeso/ic/MingesoIcApplicationTests.java
@@ -1,6 +1,5 @@
 package com.mingeso.ic;
 
-import com.mingeso.ic.services.ProductServiceTest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 //import org.junit.runners.Suite;
@@ -10,11 +9,6 @@ import org.springframework.context.annotation.ComponentScan;
 //import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
-
-@Suite.SuiteClasses({
-       ProductServiceTest.class
-})
-@ActiveProfiles("test")
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @SpringBootApplication(scanBasePackages="com.mingeso.ic")
