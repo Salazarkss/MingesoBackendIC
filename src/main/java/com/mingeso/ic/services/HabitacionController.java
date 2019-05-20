f55ba3462af940cc907e99709fe6560d3b94abfb
Sebastian Salazar V <sebastian.salazar@symbiose.cl>
2019-05-20 18:23:10 -0400

    ADD: Se agrega servicio post para nueva habitacion
    Signed-off-by: Sebastian Salazar V <sebastian.salazar@symbiose.cl>

--- src/main/java/com/mingeso/ic/services/HabitacionController.java
+++ src/main/java/com/mingeso/ic/services/HabitacionController.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -26,6 +28,19 @@ public class HabitacionController{
 		catch(Exception e){
 			return null;
 		}
+	}
+
+	@PostMapping("/habitacion/{number}/{tipo}")
+	public void insertHabitacion(@PathVariable String number, @PathVariable String tipo){
+		System.out.println(number);
+		System.out.println(tipo);
+		Habitacion hab = new Habitacion();
+		List<Habitacion> habitaciones = habitacionDAO.getAll();
+		Habitacion lastHabitacion = habitaciones.get(habitaciones.size() - 1);
+		hab.setId(lastHabitacion.getId() + 1);
+		hab.setNumber(number);
+		hab.setTipo(tipo);
+		habitacionDAO.insertHabitacion(hab);
 	}
 
 }
