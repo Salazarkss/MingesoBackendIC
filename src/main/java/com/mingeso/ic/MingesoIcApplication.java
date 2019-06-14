1da86e6c24aaf82997b5dcdee0b383fd68e4addf
Francisco <francisco.diaz.s@usach.cl>
2019-06-13 23:27:01 -0400

    FIX: Arreglo de Code Smell

--- src/main/java/com/mingeso/ic/MingesoIcApplication.java
+++ src/main/java/com/mingeso/ic/MingesoIcApplication.java
@@ -17,8 +17,6 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-import com.mingeso.ic.model.Habitacion;
-
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
