5382db93f57c1123d5e20182baea27a94ca91e44
Francisco <francisco.diaz.s@usach.cl>
2019-06-18 12:26:11 -0400

    Pruebas test controller con Mock

--- src/test/java/com/mingeso/ic/services/HabitacionControllerTest.java
+++ src/test/java/com/mingeso/ic/services/HabitacionControllerTest.java
@@ -1,18 +1,34 @@
 package com.mingeso.ic.services;
 
+import com.mingeso.ic.DAO.HabitacionDAO;
+import com.mingeso.ic.DAO.TipoDAO;
 import com.mingeso.ic.model.Habitacion;
+import org.hibernate.service.spi.ServiceException;
 import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 
 import java.util.List;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 public class HabitacionControllerTest {
-    public HabitacionController habitacionController = new HabitacionController();
+
+    @InjectMocks
+    private HabitacionController habitacionController = new HabitacionController();
+
+    @Mock
+    HabitacionDAO habitacionDAO;
+
+    @Mock
+    TipoDAO tipoDAO;
 
     @Test
-    public void controlador(){
+    public void testController() throws ServiceException {
         List<Habitacion> habitaciones = habitacionController.getHabitacionesAll();
+
         assertNotNull(habitaciones);
+        assertTrue(habitaciones.size()>0);
     }
 }
